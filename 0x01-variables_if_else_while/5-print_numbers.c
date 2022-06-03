dfe1248bbc30d249992bf8fcd3f4a13e0534da41
Hassan-Hajir <hashajir@.com>
2022-06-03 12:04:17 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - entry point
+*Description: prints number 0 to 9
+*Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
