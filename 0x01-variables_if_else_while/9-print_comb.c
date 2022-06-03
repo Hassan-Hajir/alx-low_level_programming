776b3d8935977a51b03ad53bb06d8f35a675abef
Hassan-Hajir <hashajir@.com>
2022-06-03 14:08:10 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -entry
+ *Description: prints  allpossible combination of sungle digt number
+ *Return 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= "a"; c--)
+	{
+	putchar(c);
+	}
+	putchar('\');
+	return 0;
+}
