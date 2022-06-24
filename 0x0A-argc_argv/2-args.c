779ade8fdfebc750fa774d2a2dff095a1723ec0f
Hassan-Hajir <hashajir@.com>
2022-06-24 02:42:02 -0700

    a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *main - Prints arguments 
+ *@argc: Number of arguments
+ *@argv: Array name
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
