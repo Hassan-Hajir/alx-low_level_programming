6087dc29f8e27c16d5a1a78e770e5df5836a31ed
root <hashajir@gmail.com>
2023-04-10 03:19:04 -0700

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,20 +1,17 @@
+#include "hash_tables.h"
+
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- *
- * Description: Key/value pairs are printed in the order
- *              they appear in the array of the hash table.
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
-void hash_table_print(const hash_table_t *ht)
+void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *node;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
-	int first = 1;
 
 	if (ht == NULL)
 		return;
 
-	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
@@ -22,15 +19,17 @@ void hash_table_print(const hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				if (first)
-					first = 0;
-				else
-					printf(", ");
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
+			ht->array[i] = NULL;
 		}
 	}
-	printf("}\n");
+
+	free(ht->array);
+	free(ht);
 }
 
